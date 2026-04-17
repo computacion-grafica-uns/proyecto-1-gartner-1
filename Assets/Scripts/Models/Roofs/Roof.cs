4d42999efcfd954cfd5aafe0523c6e86587aa23e
gartnerfn <gartnerfn@gmail.com>
2026-04-17 16:31:58 -0300

    feat(Roofs): add roof

---
+++ Assets/Scripts/Models/Roofs/Roof.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Roof : CustomGameObject
+{
+    protected override MeshData CreateMeshData()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+          new Vector3(-3.5f, 0, -4),
+          new Vector3(3.5f , 0, -4),
+          new Vector3(3.5f, 0, 4),
+          new Vector3(-3.5f, 0, 4),
+        };
+
+        int[] triangles = new int[]
+        {
+          0, 1, 2,
+          0, 2, 3,
+        };
+
+        Color[] colors = new Color[vertices.Length];
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = Color.gray;
+        }
+
+        return new MeshData(vertices, triangles, colors);
+    }
+    public override void Create()
+    {
+        Create("Roof");
+    }
+}
